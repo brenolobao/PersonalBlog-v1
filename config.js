9d27a43f01547b3b1cbdff68efefb85bbb01fd09
Breno Lobão Silva Barbosa <80777991+brenolobao@users.noreply.github.com>
2025-08-14 17:34:37 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC54KtNdz6Od_fK8QBx4LR72pKE7t0aBoM",
+    authDomain: "blogproject-51994.firebaseapp.com",
+    projectId: "blogproject-51994",
+    storageBucket: "blogproject-51994.firebasestorage.app",
+    messagingSenderId: "244653546636",
+    appId: "1:244653546636:web:c166dd68f11fa831fdefa9"
+};
